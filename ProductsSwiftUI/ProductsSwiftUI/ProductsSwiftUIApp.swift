b42fcc122ae4389827512c1b803f38a6276c21dc
Asad <asad.mehmood@venturedive.com>
2024-12-21 11:08:36 +0500

    Project Setup

---
+++ ProductsSwiftUI/ProductsSwiftUI/ProductsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductsSwiftUIApp.swift
+//  ProductsSwiftUI
+//
+//  Created by Asad Mehmood on 21/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ProductsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
